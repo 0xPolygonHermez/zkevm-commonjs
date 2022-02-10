1429396c929461a204825ee8154f219b1f62f87a
krlosMata <krlosmata@gmail.com>
2022-02-10 16:44:23 +0100

    add missing test

--- test/contract-utils.test.js
+++ test/contract-utils.test.js
@@ -1,28 +1,52 @@
-/*
- * const { Scalar } = require('ffjavascript');
- * const { expect } = require('chai');
- * const ethers = require('ethers');
- */
+const { expect } = require('chai');
 
-/*
- * const {
- *     contractUtils,
- * } = require('../index');
- */
+const fs = require('fs');
+const path = require('path');
 
-/*
- * describe('contractUtils', () => {
- *     before(async () => {
- *     });
- */
+const { contractUtils } = require('../index');
 
-//     it('calculateCircuitInput', async () => {
+const { pathTestVectors } = require('./helpers/test-utils');
 
-//     });
+describe('contractUtils', function () {
+    this.timeout(10000);
+    let testVector;
 
-//     it('calculateBatchHashData', async () => {
+    const expectedBatchHashData = '0x3567576c83ca658d335055b092195e2fc9d15bf20495a04f879944c160844e28';
+    const expectedGlobalHash = '0x1c0620e20e2670641adcd92a20ac534e4294638370af24f54fbd7abc2ff18c6e';
 
-/*
- *     });
- * });
- */
+    before(async () => {
+        testVector = JSON.parse(fs.readFileSync(path.join(pathTestVectors, 'inputs-executor/input_0.json')));
+    });
+
+    it('calculateBatchHashData', async () => {
+        const { batchL2Data, globalExitRoot } = testVector;
+        const computedBatchHashData = await contractUtils.calculateBatchHashData(batchL2Data, globalExitRoot);
+
+        expect(expectedBatchHashData).to.be.equal(computedBatchHashData);
+    });
+
+    it('calculateCircuitInput', async () => {
+        const {
+            numBatch,
+            sequencerAddr,
+            chainId,
+            oldLocalExitRoot,
+            newLocalExitRoot,
+            oldStateRoot,
+            newStateRoot,
+        } = testVector;
+
+        const computedGlobalHash = await contractUtils.calculateCircuitInput(
+            oldStateRoot,
+            oldLocalExitRoot,
+            newStateRoot,
+            newLocalExitRoot,
+            sequencerAddr,
+            expectedBatchHashData,
+            chainId,
+            numBatch,
+        );
+
+        expect(computedGlobalHash).to.be.equal(expectedGlobalHash);
+    });
+});
