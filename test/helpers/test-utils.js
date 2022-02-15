90be4f8ce6534a465140100d6b0043f38ece0b34
invocamanman <jesus.ligero@yahoo.es>
2022-02-15 18:37:37 +0100

    update zkEVMDB

--- test/helpers/test-utils.js
+++ test/helpers/test-utils.js
@@ -1,18 +1,6 @@
 const path = require('path');
-const { stateUtils } = require('../../index');
-
 const pathTestVectors = path.join(__dirname, '../../node_modules/@polygon-hermez/test-vectors');
 
-async function setGenesisBlock(addressArray, amountArray, nonceArray, smt) {
-    let currentRoot = smt.F.zero;
-    for (let i = 0; i < addressArray.length; i++) {
-        currentRoot = await stateUtils.setAccountState(addressArray[i], smt, currentRoot, amountArray[i], nonceArray[i]);
-    }
-
-    return currentRoot;
-}
-
 module.exports = {
-    setGenesisBlock,
     pathTestVectors,
 };
