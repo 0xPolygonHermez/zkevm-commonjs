6828d065a87a975960be96a7f148f74175210b62
Laia Soler <laia@polygon.technology>
2024-10-16 14:55:04 +0200

    fix modexp counters

--- src/virtual-counters-manager-utils.js
+++ src/virtual-counters-manager-utils.js
@@ -10,19 +10,26 @@
 
 function expectedModExpCounters(lenB, lenE, lenM, B, E, M) {
     const [Q_B_M, R_B_M] = [B / M, B % M];
-    const Bsq = B * B;
-    const [Q_Bsq_M, R_Bsq_M] = [Bsq / M, Bsq % M];
 
-    const lenE2 = Math.floor(lenE / 2) || 1;
+    const lenQE2 = Math.floor(lenE / 2) || 1;
 
-    const log2E = Math.floor(Math.log(Number(E)) / Math.log(2));
+    // const log2E = Math.floor(Math.log(Number(E)) / Math.log(2));
+
+    let nTimesOdd = 0;
+    while (E > 0n) {
+        nTimesOdd += Number(E & 1n);
+        E >>= 1n;
+    }
+    const nTimesEven = lenE * 256 - nTimesOdd;
 
     const counters = { ariths: 0, binaries: 0, steps: 0 };
     const a = setupAndFirstDivCounters();
-    const b = oddIterationCounters();
+    const b = halfLoopCounters();
+    const c = fullLoopCounters();
 
     for (const key in counters) {
-        counters[key] = a[key] + log2E * b[key];
+        // counters[key] = a[key] + log2E * b[key];
+        counters[key] = a[key] + nTimesEven * b[key] + nTimesOdd * c[key];
     }
 
     return counters;
@@ -40,54 +47,69 @@ function expectedModExpCounters(lenB, lenE, lenM, B, E, M) {
         return len;
     }
 
-    // Counters computation of the setup and first division.
+    // Counters computation of the setup and first division. + 2 last steps
     function setupAndFirstDivCounters() {
         return {
             steps:
-                74
+                84
                 + 2 // last 2 steps
                 + 10 * lenB
-                + 26 * lenM
+                + 3 * lenM
                 + (8 + 19 * lenM ) * computeLenThisBase(Q_B_M)
-                + 8 * computeLenThisBase(R_B_M),
+                + 12 * computeLenThisBase(R_B_M),
              binaries:
-                2
-                + 2 * lenM
+                4
+                - lenM
+                + computeLenThisBase(R_B_M)
                 + 2 * computeLenThisBase(Q_B_M) * lenM,
             ariths:
-                lenM 
-                + (19 * lenM - 18) * computeLenThisBase(Q_B_M)
+                lenM * computeLenThisBase(Q_B_M)
         };
     }
 
-    // Counters computation of the half loop.
-    function oddIterationCounters() {
+    function halfLoopCounters() {
         return {
             steps:
-                229
-                + 14 * lenB
+                153
+                + 82 * lenM
                 + 6 * lenE
-                + 68 * lenM
-                + 51 * lenB**2
-                + 38 * lenB * lenM
-                + 25 * lenE2
-                + (19 * lenM + 8) * computeLenThisBase(Q_Bsq_M) 
-                + 8 * computeLenThisBase(R_Bsq_M),
+                + (80 * lenM * (lenM - 1)) / 2
+                + 19 * lenM**2
+                + 25 * lenQE2,
             binaries:
-                11
-                - 9 * lenB
-                + 3 * lenM
-                + 9 * lenB**2
-                + 4 * lenB * lenM
-                + 2 * lenE2
-                + 2 * computeLenThisBase(Q_Bsq_M) * lenM,
+                9
+                + 6 * lenM
+                + (23 * lenM * (lenM - 1)) / 2
+                + 2 * lenM**2
+                + 3 * lenQE2,
             ariths:
                 - 1
-                - 16 * lenB
-                - 16 * lenM
-                + lenB**2
-                + 38 * lenB * lenM
-                + (19 * lenM - 18) * computeLenThisBase(Q_Bsq_M),
+                + 2 * lenM
+                + (2 * lenM * (lenM - 1)) / 2
+                + lenM**2
+        };
+    }
+
+    function fullLoopCounters() {
+        return {
+            steps:
+                263
+                + 114 * lenM
+                + 6 * lenE
+                + (80 * lenM * (lenM - 1)) / 2
+                + 57 * lenM**2
+                + 25 * lenQE2,
+            binaries:
+                17
+                + 3 * lenM
+                + (23 * lenM * (lenM - 1)) / 2
+                + 6 * lenM**2
+                + 3 * lenQE2,
+            ariths:
+                - 1
+                + 2 * lenM
+                + (2 * lenM * (lenM - 1)) / 2
+                + 3 * lenM**2
         };
     }
 }
